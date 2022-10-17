cc1309787d71a54079cb3c47c33cffb9eb7a2bf7
YeonwooSung <neos960518@gmail.com>
2022-10-17 14:23:48 +0900

    Add retry logic to the web application

--- public/js/ttiForm.js
+++ public/js/ttiForm.js
@@ -5,15 +5,23 @@ class TtiForm {
     constructor(uuid, text, expectedTime) {
         this.uuid = uuid;
         this.text = text;
-        this.expectedTime = Number(expectedTime) + 10;
+        this.expectedTime = Number(expectedTime) + 5;
         this.timeout_interval = this.expectedTime * 1000;
         this.status = 'pending';
+        this.retry_interval = 10000;
+        this.retry_interval_sec = 10;
+        this.max_retry = 3;
+        this.retry_count = 0;
     }
 
     async addTimeout() {
+        setTimeout(this.timeout_interval)
+    }
+
+    async setTimeout(timeout) {
         this.timeout = setTimeout(() => {
             this.sendRequestForResultImage();
-        }, this.timeout_interval);
+        }, timeout);
     }
 
     sendRequestForResultImage() {
@@ -23,7 +31,17 @@ class TtiForm {
             var img = document.getElementById('tti-image');
             img.src = `/tti/image?uuid=${this.uuid}`;
         }).catch((err) => {
-            console.log(err);
+            // console.log(err);
+            if (this.retry_count < this.max_retry) {
+                alert(`Image not generated yet.. Retry after ${this.retry_interval_sec} seconds.`);
+                this.retry_count += 1;
+                this.setTimeout(this.retry_interval);
+                this.retry_interval += 2000;
+                this.retry_interval_sec += 2;
+            } else {
+                this.status = 'failed';
+                alert(`Exceeded max retry count. Please try again later.`);
+            }
         });
     }
 }
